91c3de2a3d669b584e15589290ac06b34283aa31
Toxaencom1 <Toxaencom@gmail.com>
2022-10-14 20:20:35 +0600

    Solve second task

--- HomeWorkSem1/Program.cs
+++ HomeWorkSem1/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Привет друг, эта программа покажет какое число большее а какое меньшее");
+﻿/*
+Console.WriteLine("Привет друг, эта программа покажет какое число большее а какое меньшее");
 Console.WriteLine("Введи первое число");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введи второе число");
@@ -14,6 +15,33 @@ else
     Console.WriteLine($"<{b}> - Большее число"); 
     Console.WriteLine($"<{a}> - Меньшее число"); 
 }
+*/
 
+Console.WriteLine("Привет друг, эта программа покажет максимальное число из 3х чисел которые ты введешь");
+Console.WriteLine("Введи первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Обрабатываю запрос ... ");
 
+int max;
 
+if(a>b)
+{
+    max=a;
+    if(max<c)
+    {
+        max=c;
+    }
+}
+else
+{
+    max=b;
+    if(max<c)
+    {
+        max=c;
+    }
+}
+Console.WriteLine($"{max} - максимальное число");
