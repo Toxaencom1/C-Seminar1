d8057cf671088fc55f429d21384d85d16215b73b
Toxaencom1 <Toxaencom@gmail.com>
2022-10-14 20:03:25 +0600

    Solve first task

--- HomeWorkSem1/Program.cs
+++ HomeWorkSem1/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.WriteLine("Привет друг, эта программа покажет какое число большее а какое меньшее");
+Console.WriteLine("Введи первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Обрабатываю запрос ... ");
+if(a>b)
+{
+    Console.WriteLine($"<{a}> - Большее число"); 
+    Console.WriteLine($"<{b}> - Меньшее число");       
+}
+else
+{
+    Console.WriteLine($"<{b}> - Большее число"); 
+    Console.WriteLine($"<{a}> - Меньшее число"); 
+}
+
+
+
