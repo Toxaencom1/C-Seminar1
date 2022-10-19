7998d7296d158855a578cd0fe0899fb2f975a68e
Toxaencom1 <Toxaencom@gmail.com>
2022-10-19 14:30:40 +0600

    Solved second task <HomeWorkSem2>

--- HomeWorkSem2/Program.cs
+++ HomeWorkSem2/Program.cs
@@ -10,7 +10,7 @@ int SecondNumber(int number)
 
 Console.WriteLine("Введи трехзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
-if(num>100 && num<1000)
+if(num>=100 && num<1000)
 {
     int result = SecondNumber(num);
     Console.WriteLine($"{result} - ваша вторая цифра");
@@ -21,3 +21,45 @@ else
 }
 */
 
+/*
+// Задача №2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Здравствуй пользователь, эта программа выводит третью цифру заданного числа");
+
+int ThirdNumber(int number)
+{
+    int index=0;
+    int res=0;
+    while(index<7)
+    {
+        if(number>100 && number<1000)
+        {
+            res = number%10;
+            break;
+        }
+        else
+        {
+            number = number/10;
+        }
+        index++;
+    }
+    return res;
+}
+
+Console.WriteLine("Введи число:  P.S - не более 9 знаков ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num<0)
+    {
+        num = (-1)*num;
+    }
+if(num<100)
+{
+    Console.WriteLine("Число двузначное, третьей цифры нет!");        
+}
+else
+{
+    
+    int result = ThirdNumber(num);
+    Console.WriteLine($"{result} - ваша третья цифра");    
+}
+*/
