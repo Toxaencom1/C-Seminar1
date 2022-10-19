3eed6132d6d01394de1b3ad6f36269333b3e4080
Toxaencom1 <Toxaencom@gmail.com>
2022-10-19 13:25:47 +0600

    Solved first task <HomeWorkSem2>

--- HomeWorkSem2/Program.cs
+++ HomeWorkSem2/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿// Задача №1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+Console.WriteLine("Здравствуй пользователь, эта программа показывает вторую цифру трёхзначного числа");
+
+int SecondNumber(int number)
+{
+    int res = (number / 10)%10; 
+    return res;
+}
+
+Console.WriteLine("Введи трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num>100 && num<1000)
+{
+    int result = SecondNumber(num);
+    Console.WriteLine($"{result} - ваша вторая цифра");
+}
+else
+{
+    Console.WriteLine("Число не является трехзначным");
+}
+*/
+
