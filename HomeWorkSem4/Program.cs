81d71955ed37004f8cc2df0b668f7a331db0b41b
Toxaencom1 <Toxaencom@gmail.com>
2022-11-07 17:11:30 +0600

    Solved second task for HomeWorkSem4

--- HomeWorkSem4/Program.cs
+++ HomeWorkSem4/Program.cs
@@ -21,7 +21,38 @@ int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"А в степени В равна: {SquareNumber(a,b)}");
 */
 
-// Задача №2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// Задача №2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. (901)
+
+/*
+int SymbolsSum(int fullNumber)
+{
+    int lastNumber = 0;
+    int count = 1;
+    int result = 0;
+    while(count != 0)
+    {
+        if(fullNumber == 0)
+        {
+            break;
+        }
+        else
+        {
+            lastNumber = fullNumber;
+            lastNumber %= 10;
+            result += lastNumber;
+            fullNumber /= 10;
+        }
+        
+    }
+    return result;
+}
+
+Console.WriteLine("Привет пользователь, эта программа считает сумму всех знаков числа: Введи число не более 9и знаков");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма знаков вашего числа равна: {SymbolsSum(num)}");
+
+*/
+
 
 
 
