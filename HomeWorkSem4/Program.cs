1034157879b16c1e69d4c0a21e6d8db424fe0ae7
Toxaencom1 <Toxaencom@gmail.com>
2022-11-07 15:41:05 +0600

    Solved first task HomeWorkSem4

--- HomeWorkSem4/Program.cs
+++ HomeWorkSem4/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №1. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+/*
+int SquareNumber(int aNum, int bNum)
+{
+    int count = 1;
+    int square = bNum;
+    int result = aNum;
+    while(count<square)
+    {
+        result *= aNum;
+        count ++;
+    }
+    return result;
+}
+
+Console.WriteLine("Привет пользователь, эта программа возводит число А в число В: Введи число А");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число В: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"А в степени В равна: {SquareNumber(a,b)}");
+*/
+
+// Задача №2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+
+// Задача №3. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
