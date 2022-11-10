ce8e3e6786d7df61e397537f994630c760a41ec0
Toxaencom1 <Toxaencom@gmail.com>
2022-11-10 22:23:06 +0600

    I did many fixex for all tasks fo HomeWorkSem5

--- HomeWorkSem5/Program.cs
+++ HomeWorkSem5/Program.cs
@@ -3,32 +3,24 @@
 
 
 /*
-void ArrayOutput(int[] array) // Общий метод для первой и второй задачи!!! Как я понял перегруз методов делается в классе, с этим я не знаком, поэтому получилось неудобно
+void ArrayOutput(int[] array) // P.S Общий метод для первой и второй задачи!!! Перегрузку методов делать не умеею пока, для 3й задачи отдельно методы
 {
     Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
-    {
+    for (int i = 0; i < array.Length; i++)    
         if(i == (array.Length-1))
-        {
-            Console.Write($"{array[i]}");   
-        }
-        else
-        {
-            Console.Write($"{array[i]}, ");    
-        }
-    }
+            Console.Write($"{array[i]}");        
+        else        
+            Console.Write($"{array[i]}, ");
     Console.WriteLine("]");
 }
-*/
 
-/*
-int[] ArrayPosRandomInput100Till1000(int length)
+
+
+int[] ArrayRandomInput(int length, int a, int b) // Общий метод для первой и второй задачи!!! 
 {
     int[] array = new int[length];
-    for (int i = 0; i < length; i++)
-        {
-            array[i] = new Random().Next(100,1000);
-        }
+    for (int i = 0; i < length; i++)        
+        array[i] = new Random().Next(a,b);
     return array;
 }    
 */
@@ -37,77 +29,69 @@ int[] ArrayPosRandomInput100Till1000(int length)
 int EvenNumbersOfArrayCount(int[] array)
 {
     int count = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if(array[i]%2 == 0)
-        {
-           count++; 
-        }    
-    }
+    for (int i = 0; i < array.Length; i++)    
+        if(array[i]%2 == 0)        
+           count++;
     return count;
 }
-*/
-/*
-Console.WriteLine("Привет пользователь, эта программа считает количество случайных, четных, трехзначных элементов массива");
-Console.WriteLine("Введите размер массива: ");
-int length = Convert.ToInt32(Console.ReadLine());
-if(length<0)
-{
-    length = length * (-1);
-}
-int[] myArray = ArrayPosRandomInput100Till1000(length);
-ArrayOutput(myArray);
-Console.WriteLine($"Число четных элементов массива: {EvenNumbersOfArrayCount(myArray)} ");
 
+
+Console.WriteLine("Привет пользователь, эта программа считает количество случайных, четных, трехзначных элементов массива");
+Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+if(length<0)
+    length = length * (-1);
+
+int[] myArray = ArrayRandomInput(length, 100, 1000);
+Console.WriteLine();
+ArrayOutput(myArray);
+Console.WriteLine($"\nЧисло четных элементов массива: {EvenNumbersOfArrayCount(myArray)} \n");
 */
+
 
 // Задача №2. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
-// P.S - Для задачи требуется метод ArrayOutput
+// P.S - Для задачи требуется метод выше ArrayOutput и ArrayRandomInput
 
 /*
-int[] ArrayRandomInput(int length)
-{
-    int[] array = new int[length];
-    for (int i = 0; i < length; i++)
-        {
-            array[i] = new Random().Next(-99,100);
-        }
-    return array;
-}    
-
-
-
-int SumOfUnEvenValueIndex(int[] array)
+int SumOfUnevenValueIndex(int[] array)
 {
     int sum = 0;
-    for (int i = 1; i < array.Length; i+=2)
-    {
-        sum += array[i];
-    }
+    for (int i = 1; i < array.Length; i+=2)    
+        sum += array[i];    
     return sum;
 }
 
 Console.WriteLine("Привет пользователь, эта программа считает сумму случайных значений, нечетных позиций массива");
-Console.WriteLine("Введите размер массива: ");
+Console.Write("Введите размер массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.Write("Введите размерность элементов массива ОТ: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность элементов массива ДО: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
 
 if(length<0)
     length = length * (-1);
 
-int[] myArray = ArrayRandomInput(length);
+int[] myArray = ArrayRandomInput(length, a, b);
 ArrayOutput(myArray);
-Console.WriteLine($"Сумма элементов массива на нечетных позициях равна: {SumOfUnEvenValueIndex(myArray)}");
+Console.WriteLine($"\nСумма элементов массива на нечетных позициях равна: {SumOfUnevenValueIndex(myArray)}\n");
 */
 
 // Задача №3. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
+/*
 double[] ArrayDoubleRandomInput(int length)
 {
     double[] array = new double[length];
     for (int i = 0; i < length; i++)
-        {
-            
+        {            
             array[i] = new Random().Next(-100,100);
             array[i] += new Random().NextDouble();
             array[i] = Math.Round(array[i], 2);
@@ -122,14 +106,10 @@ double MaxMinDiff(double[] array)
     double maxValue = array[0];
     for (int i = 1; i < array.Length; i++)
     {
-        if(array[i] < minValue)
-        {
-            minValue = array[i];
-        }
-        if(array[i] > maxValue)
-        {
-            maxValue = array[i];
-        }
+        if(array[i] < minValue)        
+            minValue = array[i];        
+        if(array[i] > maxValue)        
+            maxValue = array[i];        
     }
     double result = maxValue - minValue;
     return result; 
@@ -141,25 +121,23 @@ void ArrayOutputDouble(double[] array)
     for (int i = 0; i < array.Length; i++)
     {
         if(i == (array.Length-1))
-        {
-            Console.Write($"{array[i]}");   
-        }
+            Console.Write($"{array[i]}");        
         else
-        {
-            Console.Write($"{array[i]} ; ");    
-        }
+            Console.Write($"{array[i]} ; ");
     }
     Console.WriteLine("]");
 }
 
 Console.WriteLine("Привет пользователь, эта программа считает разницу случайных вещественных значений,\nмежду минималным и максимальным значнением  массива");
-Console.WriteLine("Введите размер массива: ");
+Console.Write("Введите размер массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
 
 if(length<0)
     length = length * (-1);
 
+Console.WriteLine();
+
 double[] myArray = ArrayDoubleRandomInput(length);
 ArrayOutputDouble(myArray);
-
-Console.WriteLine($"Разница между минимальным и максимальным равна: {Math.Round(MaxMinDiff(myArray),2)}");
+Console.WriteLine($"\nРазница между минимальным и максимальным равна: {Math.Round(MaxMinDiff(myArray),2)}\n");
+*/
