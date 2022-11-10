445c9f5ce7d2c799b7200d0074954a5161703282
Toxaencom1 <Toxaencom@gmail.com>
2022-11-10 18:32:37 +0600

    Solved first task for HomeWorkSem5

--- HomeWorkSem5/Program.cs
+++ HomeWorkSem5/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №1. Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в массиве.
+
+void ArrayOutput(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i == (array.Length-1))
+        {
+            Console.Write($"{array[i]}");   
+        }
+        else
+        {
+            Console.Write($"{array[i]}, ");    
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] ArrayPosRandomInput100Till1000(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+        {
+            array[i] = new Random().Next(100,1000);
+        }
+    return array;
+}    
+
+int EvenNumbersOfArrayCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]%2 == 0)
+        {
+           count++; 
+        }    
+    }
+    return count;
+}
+
+Console.WriteLine("Привет пользователь, эта программа считает количество случайных, четных, трехзначных элементов массива");
+Console.WriteLine("Введите число - длины массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+if(length<0)
+{
+    length = length * (-1);
+}
+int[] myArray = ArrayPosRandomInput100Till1000(length);
+ArrayOutput(myArray);
+Console.WriteLine($"Число четных элементов массива: {EvenNumbersOfArrayCount(myArray)} ");
+
+
+
+// Задача №2. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// Задача №3. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
