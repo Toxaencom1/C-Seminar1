f6038d00d240abbebe10d83907606b650a71c330
Toxaencom1 <Toxaencom@gmail.com>
2022-11-21 16:38:39 +0600

    Solved First task

--- HomeWorkSem8/Program.cs
+++ HomeWorkSem8/Program.cs
@@ -1,11 +1,65 @@
 ﻿// Задача №1. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-/*    1 4 7 2
-    5 9 2 3
-    8 4 2 4
-    В итоге получается вот такой массив:
-    7 4 2 1
-    9 5 3 2
-    8 4 4 2
+
+int[,] GenerateRandom2dArray(int rows, int columns, int minValue, int maxValue) // Заполнить двумерный массив
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void Output2dArray(int[,] array)                              // Вывести двумерный массив
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0}\t", array[i, j]);
+        }
+        Console.WriteLine(" ]");
+    }
+}
+
+/*
+void SortRowsOfMassive(int[,] array)             // Основной метод решения задачи
+{    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                if(array[i,k]<array[i,j])
+                {
+                int temp = array[i,j];
+                array[i,j] = array[i,k];
+                array[i,k] = temp;
+                }                
+            }
+        }
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите размерность элементов ОТ: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите размерность элементов ДО: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = GenerateRandom2dArray(m, n, min, max);
+
+Output2dArray(myArray);
+Console.WriteLine();
+SortRowsOfMassive(myArray);
+Output2dArray(myArray);
 */
 
 // Задача №2. Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
