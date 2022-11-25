ecd817177813675ed3a70f960e7d2866cabba628
Toxaencom1 <Toxaencom@gmail.com>
2022-11-25 11:51:50 +0600

    Solved last task for HomeWorkSem9

--- HomeWorkSem9/Program.cs
+++ HomeWorkSem9/Program.cs
@@ -41,28 +41,30 @@ Console.WriteLine($"Сумма натуральных чисел равна: {Su
 
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Задача №3. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
 /*
-m = 2, n = 3 -> A(m,n) = 9
-m = 3, n = 2 -> A(m,n) = 29
+uint AckermanFunction(uint m, uint n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m != 0 && n == 0)
+        return AckermanFunction(m - 1, 1);
+    else
+        return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+}
+
+Console.Clear();
+
+Console.WriteLine("Здравствуй пользователь, эта программа вычисления функции Аккермана с помощью рекурсии");
+Console.WriteLine("Введите положительные числа M и N");
+
+Console.WriteLine("Введите M");
+uint m = Convert.ToUInt32(Console.ReadLine());
+
+Console.WriteLine("Введите N");
+uint n = Convert.ToUInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine($"Вычисление функции Аккермана равны: {AckermanFunction(m, n)}");
 */
